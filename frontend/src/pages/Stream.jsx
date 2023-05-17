6a17df2ab89e95ebf85fd9ca4036b39db2645e0f
Sebi Dancau <sebidancau@yahoo.com>
2023-05-17 19:46:40 +0300

    trying to fix ws one more time

--- frontend/src/pages/Stream.jsx
+++ frontend/src/pages/Stream.jsx
@@ -21,7 +21,7 @@ import startPublish from '../webrtc/startPublish';
 
 flowplayer(HLSPlugin)
 
-const socket = io.connect('http://localhost:4000');
+const socket = io.connect('https://leven-tv.com');
 
 function Stream() {
     const navigate = useNavigate()
